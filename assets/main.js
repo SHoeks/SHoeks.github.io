805e35af376fc059e2d5b12e92d838b2b568d800
SHoeks <36075931+SHoeks@users.noreply.github.com>
2020-12-04 17:43:38 +0100

    layout updated, inspired by instagram

--- assets/main.js
+++ assets/main.js
@@ -1,16 +1,3 @@
-// data holding image names and description, first entry is ment as example
-var data = [ 
-    ["000.jpg", "Image description"],
-    ["001.jpg", "Passage into the unknown"],
-    ["002.jpg", "Beyond the glimmer of another world"],
-    ["003.jpg", "Here we may reign secure"],
-    ["004.jpg", "There is none else walking"],
-    ["007.jpg", "Smooth it glides upon its travel"],
-    ["006.jpg", "The pathway to the hidden falls"],
-    ["008.jpg", "..."],
-    ["005.jpg", "I heard the dingoes cry"],
-];
-
 // available quality folders
 var quality = ["images/jpg_800x800/","images/jpg_1500x1500/","images/jpg_2000x2000/"];
 
@@ -61,9 +48,7 @@ function set_image_in_overlay(dir,n) {
     document.getElementById("img_show").src = dir + data[n][0];
     document.getElementById("overlay").style.display = "block";
     document.getElementById("logo").style.opacity = 0.5;
-    setTimeout(function() {
-        document.getElementById("img_description").innerHTML = data[n][1];
-    }, 500);
+    document.getElementById("img_description").innerHTML = data[n][1];
 }
 
 // function for updating the prev and next indices
