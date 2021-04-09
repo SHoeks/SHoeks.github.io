e24d7df41e98422a354ba04da4c12aa348d18768
SHoeks <36075931+SHoeks@users.noreply.github.com>
2021-04-09 19:14:54 +0200

    site updated

---
+++ minimal/data.js
@@ -0,0 +1,32 @@
+// data holding image names and description, first entry is ment as example
+var data = [ 
+  ["https://live.staticflickr.com/65535/51066364448_971b4a8eea_w.jpg", "Image description"],
+  ["https://live.staticflickr.com/65535/51066364448_971b4a8eea_w.jpg", "Passage into the unknown"],
+  ["https://live.staticflickr.com/65535/51066364448_971b4a8eea_w.jpg", "Beyond the glimmer of another world"],
+  ["003.jpg", "Here we may reign secure"],
+  ["004.jpg", "There is none else walking"],
+  ["007.jpg", "Smooth it glides upon its travel"],
+  ["008.jpg", "..."],
+  ["006.jpg", "The pathway to the hidden falls"],
+  ["017.jpg", "..."],
+  ["009.jpg", "..."],
+  ["010.jpg", "..."],
+  ["011.jpg", "..."],
+  ["005.jpg", "I heard the dingoes cry"],
+  ["012.jpg", "..."],
+  ["013.jpg", "..."],
+  ["014.jpg", "..."],
+  ["015.jpg", "..."],
+  ["016.jpg", "..."],
+];
+var data2 = [ 
+  ["000.jpg", "Image description"],
+  ["001.jpg", "..."],
+  ["002.jpg", "..."],
+  ["003.jpg", "..."],
+  ["004.jpg", "..."]
+];
+
+// vector holding available image qualities
+var quality = ["","",""];
+
