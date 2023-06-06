59166d3ffa2aae04342fdbee98f02370a71b99df
Colin <selwynhoeks@gmail.com>
2023-06-06 19:58:29 +0200

    2023_06_06

--- film/README.md
+++
@@ -1,3 +0,0 @@
-# FlickrAlbumAPI
-
-Test: [https://shoeks.github.io/WEBSITE_FlickrAlbumAPI/film_color.html](https://shoeks.github.io/WEBSITE_FlickrAlbumAPI/film_color.html)
--- film/assets/code/functions.js
+++
@@ -1,5 +0,0 @@
-function is_touch_enabled() {
-    return ( 'ontouchstart' in window ) ||
-           ( navigator.maxTouchPoints > 0 ) ||
-           ( navigator.msMaxTouchPoints > 0 );
-}
